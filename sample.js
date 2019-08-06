525c270acbcd568b2aba544547837e45b5706f87
Brian Shannon <brian.shann@gmail.com>
2019-08-06 11:27:05 +0100

    fixed up sample output

--- sample.js
+++ sample.js
@@ -5,19 +5,19 @@ scribbles.info('HelloWorld');
 scribbles.log('HelloWorld');
 scribbles.warn('HelloWorld');
 scribbles.error('HelloWorld');
-
+console.log();
 scribbles.config({
   logLevel:'warning',
   levels:['fatal','error','warning','info'],
   stdOut:null,
-  dataOut: data => console.log(data+'',data.status)
+  dataOut: data => data.status ? console.log(data+'',data.status) : console.log(data+'')
 })
 
-scribbles.status();
+scribbles.status("Im async");
 
-scribbles.trace('sample_trace',()=>{
   scribbles.info('A');
   scribbles.warning('B');
+scribbles.trace('in_trace',()=>{
   scribbles.error('C');
-  scribbles.fatal('B');
+  scribbles.fatal('D');
 })
